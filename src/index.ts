661dd2d8963abc8b20794ac5c619a47f3b0dbc2b
xiao <183627391@qq.com>
2021-12-21 22:10:14 +0800

    add helpers folder

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
+import { buildUrl } from './helpers/url'
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+}
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildUrl(url, params)
 }
 
 export default axios;
