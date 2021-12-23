b595d599c945b798120d46a8a0f56d943309ad2a
xiao <183627391@qq.com>
2021-12-23 19:53:55 +0800

    opt auto gene api path

--- examples/api/apiRouter.js
+++ examples/api/apiRouter.js
@@ -3,9 +3,10 @@ const path = require('path')
 const router = require('express').Router()
 
 // 读取modules文件，生成api
+const fileExtList = ['.js']
 const dirPath = path.join(__dirname, 'modules')
-fs.readdirSync(dirPath).forEach(file => {
-  require(`${dirPath}/${file}`)(router)
-})
+fs.readdirSync(dirPath)
+  .filter(file => fileExtList.includes(path.extname(file).toLowerCase()))
+  .forEach(file => require(`${dirPath}/${file}`)(router))
 
 module.exports = router
