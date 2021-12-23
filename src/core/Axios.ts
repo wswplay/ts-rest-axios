cfa7d86a26c3d2b911c3ef5e077a72c3d35df957
xiao <183627391@qq.com>
2021-12-23 20:28:51 +0800

    add Axios class

---
+++ src/core/Axios.ts
@@ -0,0 +1,45 @@
+import { AxiosPromise, AxiosRequestConfig, Method } from "../types";
+import dispatchRequest from "./dispatchRequest";
+
+export default class Axios {
+  // 基本请求方法
+  request(url: any, config?: any): AxiosPromise {
+    if (typeof url === 'string') {
+      if (!config) config = {}
+      config.url = url
+    } else {
+      config = url
+    }
+    return dispatchRequest(config)
+  }
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestWithoutData('get', url, config)
+  }
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestWithoutData('delete', url, config)
+  }
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestWithoutData('head', url, config)
+  }
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestWithoutData('options', url, config)
+  }
+  post(url: string, data?: any, config?: AxiosRequestConfig) {
+    this._requestWithData('post', url, data, config)
+  }
+  put(url: string, data?: any, config?: AxiosRequestConfig) {
+    this._requestWithData('put', url, data, config)
+  }
+  patch(url: string, data?: any, config?: AxiosRequestConfig) {
+    this._requestWithData('patch', url, data, config)
+  }
+
+  // 无data公用方法
+  _requestWithoutData(method: Method, url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this.request(Object.assign(config || {}, { method, url }))
+  }
+  // 有data公用方法
+  _requestWithData(method: Method, url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this.request(Object.assign(config || {}, { method, url, data }))
+  }
+}
