4ac3b8f9d9f1210489eefea5ef1b3961d4c188e4
xiao <183627391@qq.com>
2021-12-27 21:16:39 +0800

    css and style loader version for nprogress

--- examples/views/fileload/app.ts
+++ examples/views/fileload/app.ts
@@ -1,13 +1,38 @@
 import axios from '../../../src/index'
-// import 'nprogress/nprogress.css'
+import 'nprogress/nprogress.css'
 import NProgress from 'nprogress'
 
 const instance = axios.create()
 
+// 进度条
+loadProgressBar()
+
+// 下载
+const picUrl = 'https://cn.bing.com/th?id=OHR.Rauchnachte_ZH-CN6061051054_1920x1080.jpg&rf=LaDigue_1920x1080.jpg'
+document.getElementById('downPic').setAttribute('src', picUrl)
+
+const downloadEl = document.getElementById('download')
+downloadEl!.addEventListener('click', e => {
+  instance.get(picUrl).then(res => {
+    console.log('res===', res)
+  })
+})
+
+// 上传
+const uploadEl = document.getElementById('upload')
+uploadEl!.addEventListener('click', e => {
+  const data = new FormData()
+  const fileEl = document.getElementById('file') as HTMLInputElement
+  if (fileEl.files) {
+    data.append('file', fileEl.files[0])
+
+    instance.post('/more/upload', data)
+  }
+})
+
 function calculatePercentage(loaded: number, total: number) {
   return Math.floor(loaded * 1.0) / total
 }
-
 function loadProgressBar() {
   const setupStartProgress = () => {
     instance.interceptors.request.use(config => {
@@ -39,23 +64,3 @@ function loadProgressBar() {
   setupUpdateProgress()
   setupStopProgress()
 }
-
-loadProgressBar()
-
-const downloadEl = document.getElementById('download')
-
-downloadEl!.addEventListener('click', e => {
-  instance.get('https://cn.bing.com/th?id=OHR.Rauchnachte_ZH-CN6061051054_1920x1080.jpg&rf=LaDigue_1920x1080.jpg')
-})
-
-const uploadEl = document.getElementById('upload')
-
-uploadEl!.addEventListener('click', e => {
-  const data = new FormData()
-  const fileEl = document.getElementById('file') as HTMLInputElement
-  if (fileEl.files) {
-    data.append('file', fileEl.files[0])
-
-    instance.post('/more/upload', data)
-  }
-})
